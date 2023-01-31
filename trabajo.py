111e9ac8959186eba8db39b040ce613ad1adb99a
ambar <115164901+ambaritoo@users.noreply.github.com>
2023-01-31 11:14:22 -0600

    Add files via upload

---
+++ trabajo.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print "Ingresa un numero para saber si es par o impar"
+	nro = raw_input()
+	if (nro%2)==0:
+		print "es par"
+	else:
+		print "es impar"
+
